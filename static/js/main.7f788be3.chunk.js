b8f288d668c8948f2a27b091d753c2e1edb914cf
Andrei Dimaano <andreiidimaano@gmail.com>
2021-03-24 17:36:26 -0700

    Updates

---
+++ static/js/main.7f788be3.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcratest=this.webpackJsonpcratest||[]).push([[0],{21:function(t,e,c){},22:function(t,e,c){},29:function(t,e,c){"use strict";c.r(e);c(0);var n=c(15),r=c.n(n),s=(c(21),c(22),c(1)),i=c(6),o=function(){return Object(i.jsx)("div",{children:"hello world"})};var a=function(){return Object(i.jsx)("div",{className:"App",children:Object(i.jsx)(s.c,{children:Object(i.jsx)(s.a,{path:"/",component:o})})})},j=c(11);r.a.render(Object(i.jsx)(j.a,{children:Object(i.jsx)(a,{})}),document.getElementById("root"))}},[[29,1,2]]]);
+//# sourceMappingURL=main.7f788be3.chunk.js.map
