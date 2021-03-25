3ede5075d7c6439f819db55987b6821471e5f904
Andrei Dimaano <andreiidimaano@gmail.com>
2021-03-24 17:43:56 -0700

    add pages

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Switch, Route } from "react-router-dom";
+import { Home } from './pages';
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/" component={Home}/>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
